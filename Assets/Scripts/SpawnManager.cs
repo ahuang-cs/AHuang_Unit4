f220a8205f854bebf9e83923fd204abb9b1426c3
Andrew Huang <ahuang@sdccd.edu>
2021-04-14 15:33:35 -0700

    Added rubric requirement for "The SpawnManager script saves the random enemy location in a local variable for readability. "

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,6 +7,8 @@ public class SpawnManager : MonoBehaviour
     public float spawnRadius = 8.5f;
     public GameObject enemyPrefab;
 
+    private Vector3 randomPos;
+
     private Vector3 getRandomSpawnPosition()
     {
         return new Vector3(Random.Range(-spawnRadius, spawnRadius), enemyPrefab.transform.position.y, Random.Range(-spawnRadius, spawnRadius));
@@ -14,8 +16,8 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-        Instantiate(enemyPrefab, getRandomSpawnPosition(), enemyPrefab.transform.rotation);
+        randomPos = getRandomSpawnPosition();
+        Instantiate(enemyPrefab, randomPos, enemyPrefab.transform.rotation);
     }
 
     // Update is called once per frame
