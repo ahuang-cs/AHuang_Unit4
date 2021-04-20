73b800f5a40508418c0f0df339dcce3d8aa12fff
Andrew Huang <ahuang@sdccd.edu>
2021-04-19 19:17:01 -0700

    Unit 4: Lesson C

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 40f;
+    public float powerUpSpeed = 10f;
+    public float powerUpSecs = 8f;
+    public GameObject powerUpIndicator;
 
     private float fwdInput;
     private float fwdMagnitude;
@@ -12,6 +14,10 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private GameObject focalPoint;
     private Renderer rndr;
+    
+
+    private bool isPoweredUp = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,5 +34,39 @@ public class PlayerController : MonoBehaviour
         colorMagnitude = Mathf.Abs(fwdInput);
         rb.AddForce(focalPoint.transform.forward * fwdMagnitude, ForceMode.Force);
         rndr.material.color = new Color(1f - colorMagnitude, 1f,1f - colorMagnitude);
+        
+        powerUpIndicator.transform.position = transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("PowerUp"))
+        {
+            isPoweredUp = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpTimer());
+            powerUpIndicator.SetActive(true);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(isPoweredUp && collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Player collided with " + collision.gameObject + " while Powered Up");
+            
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 repulsionDirection = collision.gameObject.transform.position - transform.position;
+
+            enemyRb.AddForce(repulsionDirection * powerUpSpeed, ForceMode.Impulse);
+        }
+    }
+
+    private IEnumerator PowerUpTimer()
+    {
+        Debug.Log("PowerUpTimer started for " + powerUpSecs + " secs!");
+        yield return new WaitForSeconds(powerUpSecs);
+        isPoweredUp = false;
+        powerUpIndicator.SetActive(false);
     }
 }
